a27d635a812022baf3544f0e0aedd8a39471002c
Rayian Mahi <rbsmahi@gmail.com>
2020-03-04 10:18:47 +0600

    Offline detection added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import com.google.gson.Gson
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.UnknownHostException
 
 fun main(){
     while (true) {
@@ -15,23 +16,26 @@ fun sendGet(city :String) {
     var jsonData :String? = null
     val apiKey = "ec2f120dbab4d09f4d7d32eb94ac092a"
     val url = URL("http://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey")
-
-    with(url.openConnection() as HttpURLConnection) {
-        when {
-            responseCode == 404 -> {
-                println("\nLocation Not Found!\n")
-            }
-            responseCode != 200 -> {
-                println("\nURL : $url; Response Code : $responseCode\n")
-            }
-            else -> {
-                inputStream.bufferedReader().use { bufferedReader ->
-                    bufferedReader.lines().forEach {
-                        jsonData = it}
+    try {
+        with(url.openConnection() as HttpURLConnection) {
+            when {
+                responseCode == 404 -> {
+                    println("\nLocation Not Found!\n")
+                }
+                responseCode != 200 -> {
+                    println("\nURL : $url; Response Code : $responseCode\n")
+                }
+                else -> {
+                    inputStream.bufferedReader().use { bufferedReader ->
+                        bufferedReader.lines().forEach {
+                            jsonData = it}
+                    }
                 }
             }
         }
-    }
+    } catch (e: UnknownHostException) {println("\nYou're Currently Offline\n")}
+
+
     if (!jsonData.isNullOrEmpty()){
         val weatherReport = Gson().fromJson(jsonData, JsonParser::class.java)
         println("\nThe Weather in ${weatherReport.name} is ${weatherReport.main.temp_celcius}°C\nFeels like ${weatherReport.main.feels_like_celcius}°C\n")
